fd9d548afdd044bbc05c82569d5607ca171f57ee
Grachev Maksim <magra85@gmail.com>
2021-12-27 02:22:00 +0300

    Исправленый вариант

--- cook_book.py
+++ cook_book.py
@@ -1,21 +1,24 @@
-with open("cook_book.txt", "r", encoding="utf-8") as file:
-    cook_book_dict = {}
-    cook_book_keys = ["имя ингредиента", "количество", "единица измерения"]
-    for line in file:
-        dish_name = line.strip()
-        counter = int(file.readline())
-        dish_ingr_list = []
-        for _ in range(counter):
-            ingr = file.readline().strip().split("|")
-            ing_dict = dict(zip(cook_book_keys, ingr))
-            dish_ingr_list.append(ing_dict)
-        file.readline()
-        cook_book_dict[dish_name] = dish_ingr_list
+from pprint import pprint
+
+def cook_book():
+    with open('cook_book.txt', encoding='UTF-8') as file:
+        cook_book = {}
+        for line in file:
+            dish_name = line.strip()
+            cook_book[dish_name] = []
+            counter = int(file.readline().strip())
+            for _ in range(counter):
+                ingredient = file.readline().strip().split('|')
+                temp_dict = {'имя ингредиента': ingredient[0], 'количество': ingredient[1], 'единица измерения': ingredient[2]}
+                cook_book[dish_name].append(temp_dict)
+            file.readline()
+        return cook_book
+
 
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
     for dish in dishes:
-        for ing in cook_book_dict[dish]:
+        for ing in cook_book()[dish]:
             ing_name = ing["имя ингредиента"]
             if ing_name not in shop_list:
                 shop_list[ing_name] = {"количество": int(ing["количество"]) * person_count, "единица измерения": ing["единица измерения"]}
@@ -23,10 +26,6 @@ def get_shop_list_by_dishes(dishes, person_count):
                 shop_list[ing_name]["количество"] += int(ing["количество"]) * person_count
     return shop_list
 
-from pprint import pprint
-cook_book_keys
-pprint(cook_book_dict)
+pprint(cook_book())
 print()
-from pprint import pprint
-get_shop_list_by_dishes
 pprint(get_shop_list_by_dishes(["Омлет", "Запеченный картофель"], 3))
